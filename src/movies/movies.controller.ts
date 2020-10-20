57bb2283004e98a20db5ac7a6fe3585695caef3a
minung.han <hmu332233@gmail.com>
2020-10-21 00:47:23 +0900

    2.2 Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,14 +8,19 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
+  @Get('/search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a mae atfer: ${searchingYear}`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') moviceId: string) {
     return `This will return one movie with the id: ${moviceId}`;
   }
 
   @Post()
-  createMovie() {
-    return 'This will create a movie';
+  createMovie(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -23,8 +28,11 @@ export class MoviesController {
     return `This will delete a moive with the id: ${movieId}`;
   }
 
-  @Put('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will update a moive with the id: ${movieId}`;
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
