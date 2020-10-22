3b4bcf229fbfceb1bf87c67ba16f69ab8d326ffb
minung.han <hmu332233@gmail.com>
2020-10-23 00:38:46 +0900

    2.4 DTOs and Validation part One

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 허용된 애들을 거르고 통과시킴
+      forbidNonWhitelisted: true, // 이상한걸 보내면 request를 막음
+      transform: true, // 지정해둔 타입으로 변경해줌
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -12,27 +13,27 @@ export class MoviesController {
   }
 
   @Get('/search')
-  search(@Query('year') searchingYear: string) {
+  search(@Query('year') searchingYear: number) {
     return `We are searching for a movie made after: ${searchingYear}`;
   }
 
   @Get('/:id')
-  getOne(@Param('id') moviceId: string) {
+  getOne(@Param('id') moviceId: number) {
     return this.moviesService.getOne(moviceId);
   }
 
   @Post()
-  createMovie(@Body() movieData) {
+  createMovie(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData) {
     return this.moviesService.update(movieId, updateData);
   }
 }
